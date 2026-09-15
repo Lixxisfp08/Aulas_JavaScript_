93cd53f1c0a7b483d99c4ef1ab3229727af1b308
Felixx <l4garta.2023@gmail.com>
2026-09-15 11:38:46 -0300

    Adicionada a pasta 15-09 - variaveis

---
+++ 15-09/Whatsupman.js
@@ -0,0 +1,37 @@
+let Mãezinha = "Já colocou seu uniforme pra lavar?";
+let Telefone = 99999999999;
+let Numero2 = "99999999999";
+let Idade = 18;
+
+console.log( "Minha mãe sempre fala quando em casa na sexta: " + Mãezinha);
+
+console.log("Minha mãe pede pra mim confirmar meu telefone. Ela diz: Seu telefone é " + Telefone);
+
+console.log("Minha idade em 2028 será de: " + Idade);
+
+// const Telefone = 99999999999;
+// Telefone = 1234567890;
+
+// Criar 4 variáveis contendo: nome, sobrenome, idade e curso e exiba no console em uma única frase
+
+let nome = "Isaac";
+let sobrenome = "Rosa da Silva";
+let idade = "17";
+let curso = "HTML";
+
+console.log("A variável nome contém: " + nome + " e é do tipo: " + typeof nome);
+console.log("A variável sobrenome contém: " + sobrenome + " e é do tipo: " + typeof sobrenome);
+console.log("A variável idade contém: " + idade + " e é do tipo: " + typeof idade);
+console.log("A variável curso contém: " + curso + " e é do tipo: " + typeof curso);
+
+console.log("Olá, Me chamo " + nome + " " + sobrenome + " tenho " + idade + " anos e faço o curso de " + curso + ". Muito prazer em conhecê-los.")
+
+let nome2 = "Felixx";
+let peso = 50;
+let altura = 1.62;
+let funcionario = false;
+
+console.log(typeof nome2);
+console.log(typeof peso);
+console.log(typeof altura);
+console.log(typeof funcionario);
